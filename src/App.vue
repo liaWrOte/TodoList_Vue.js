36b1351a574cb2f2be70a0d66cae02fa036cd423
sandrine <liaworkingonline@gmail.com>
2021-02-26 18:56:05 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <todos></todos>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+
+export default {
+  components: {
+    Todos
+  }
+}
+</script>
---
+++ src/components/Todos.vue
@@ -0,0 +1,101 @@
+<template>
+  <section class="todoapp">
+    <header class="header">
+      <h1>Todos</h1>
+      <input type="text" class="new-todo" placeholder="Ajouter une tâche" v-model="newTodo" @keyup.enter="addTodo">
+    </header>
+    <div class="main">
+      <input type="checkbox" v-model="allDone">
+      <ul class="todo-list">
+        <li class="todo" v-for="todo in filteredTodos" :key="todo.name" :class="{completed: todo.completed, editing: todo === editing}">
+          <div class="view">
+            <input type="checkbox" v-model="todo.completed">
+            <label @dblclick="editTodo(todo)">
+              {{ todo.name}}
+            </label>
+
+            <button class="destroy" @click.prevent="deleteTodo(todo)">x</button>
+          </div>
+          <input type="text" class="edit" v-model="todo.name">
+        </li>
+
+      </ul>
+    </div>
+    <footer class="footer" v-show="todos.length > 0">
+      <span class="todo-count">
+        <strong>
+          Il vous reste {{ remaining }} tâche<span v-if="todos.length > 1">s</span>.
+          </strong>
+      </span>
+      <ul>
+        <li><a href="#" :class="{selected: filter === 'all'}" @click.prevent="filter = 'all'">Toutes<span v-if="todos.length > 0"> ({{ todos.length }})</span></a></li>
+        <li><a href="#" :class="{selected: filter === 'todo'}" @click.prevent="filter = 'todo'">A faire<span v-if="todos.length > 0"> ({{ remaining }})</span></a></li>
+        <li><a href="#" :class="{selected: filter === 'done'}" @click.prevent="filter = 'done'">Faites<span v-if="todos.length > 0"> ({{ completed }})</span></a></li>
+      </ul>
+      <button @click.prevent="deleteDone" v-show="completed">Supprimer les tâches terminées</button>
+    </footer>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      todos: [{
+        name: 'Tâche de test',
+        completed: false
+      }],
+      newTodo: '',
+      filter: 'all',
+      editing: null
+    }
+  },
+  methods: {
+    addTodo () {
+      this.todos.push({
+        completed: false,
+        name: this.newTodo
+      })
+      this.newTodo = ''
+    },
+    deleteTodo (todo) {
+      this.todos = this.todos.filter(i => i !== todo)
+    },
+    deleteDone () {
+      this.todos = this.todos.filter(i => i.completed === !true)
+    },
+    editTodo (todo) {
+      this.editing = todo
+    }
+  },
+  computed: {
+    allDone: {
+      get () {
+        return this.remaining === 0
+      },
+      set (value) {
+        this.todos.forEach(todo => {
+          todo.completed = value
+        })
+      }
+    },
+    remaining () {
+      return this.todos.filter(todo => !todo.completed).length
+    },
+    completed () {
+      return this.todos.filter(todo => todo.completed).length
+    },
+
+    filteredTodos () {
+      if (this.filter === 'todo') {
+        return this.todos.filter(todo => !todo.completed)
+      } else if (this.filter === 'done') {
+        return this.todos.filter(todo => todo.completed)
+      }
+      return this.todos
+    }
+  }
+}
+</script>
+
+<style src="./todos.css"></style>
