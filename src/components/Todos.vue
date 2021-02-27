30ff1e845a69aac9c1ee499102a310c5f197c4de
sandrine <liaworkingonline@gmail.com>
2021-02-27 11:30:13 +0100

    all done checkbox ok

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -16,32 +16,21 @@
     <div class="main">
       <p class="tasksIntro">Mes tâches</p>
 
+
       <ul class="todo-list">
+        <div class="selectAll">
+          <input type="checkbox" v-model="allDone">
+          <span>Tout sélectionner</span>
+          <hr>
+
+        </div>
         <li class="todo" v-for="todo in filteredTodos" :key="todo.name" :class="{completed: todo.completed, editing: todo === editing}">
           <div class="view">
-
             <div class="task">
-              <div class="checkboxContainer">
+
             <input type="checkbox" v-model="todo.completed" class="checkbox">
-            <span class="overlay" v-if="completed" @click="todo.completed">
-              <svg
-         xmlns="http://www.w3.org/2000/svg"
-         width="24"
-         height="24"
-         viewBox="0 0 24 24"
-         fill="none"
-         stroke="currentColor"
-         stroke-width="2"
-         stroke-linecap="round"
-         stroke-linejoin="round"
-         stroke-dasharray="24"
-         stroke-dashoffset="-24"
-         class="icon">
-              <polyline points="20 6 9 17 4 12"/>
-          </svg>
-        </span>
-            </div>
-            <span contenteditable="true" v-on:keydown.enter="doneEdit" class="taskItem">
+
+            <span contenteditable="true" v-on:keydown.enter="doneEdit">
               {{ todo.name}}
             </span>
             </div>
@@ -56,7 +45,6 @@
       </ul>
     </div>
     <footer class="footer" v-show="todos.length > 0">
-      <input type="checkbox" v-model="allDone">
       <ul>
         <li><a href="#" :class="{selected: filter === 'all'}" @click.prevent="filter = 'all'">Toutes<span v-if="todos.length > 0"> ({{ todos.length }})</span></a></li>
         <li><a href="#" :class="{selected: filter === 'todo'}" @click.prevent="filter = 'todo'">A faire<span v-if="todos.length > 0"> ({{ remaining }})</span></a></li>
@@ -100,6 +88,12 @@ export default {
     doneEdit (e) {
       this.editing = null
       e.preventDefault()
+    },
+    deleteAll () {
+      this.todos = [{
+        name: '',
+        completed: ''
+      }]
     }
   },
   computed: {
